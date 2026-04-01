5b5b1ee80800f18df6df38cd3e420b2881ca3950
wseguin <william@qrcoats.com>
2026-04-01 16:57:22 -0400

    fix: replace plus icon with downward arrow on PDF download button

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -19,8 +19,8 @@ function PdfIcon() {
     >
       <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
       <polyline points="14 2 14 8 20 8" />
-      <line x1="12" y1="18" x2="12" y2="12" />
-      <line x1="9" y1="15" x2="15" y2="15" />
+      <line x1="12" y1="11" x2="12" y2="17" />
+      <polyline points="9 14 12 17 15 14" />
     </svg>
   );
 }
